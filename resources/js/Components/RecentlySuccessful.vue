bc4610af717d91a5e4a67cc069454d2152747f09
Igor Silva <igor.alexander.silva@gmail.com>
2021-05-10 10:17:02 -0400

    Criando componente de recentlySuccessful

---
+++ resources/js/Components/RecentlySuccessful.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    v-show="message"
+    class="py-4 p-3 mb-4 close cursor-pointer flex items-center justify-between w-full bg-green-200 shadow rounded"
+  >
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+};
+</script>
