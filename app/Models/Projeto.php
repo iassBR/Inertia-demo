0c7411ad72d1c23dedc6e8bdb8548ee0235f37bc
Igor Alexander Silva <igor.alexander.silva@gmail.com>
2021-05-11 15:09:08 -0400

    Definindo accessor de data e eager loading de tarefas

--- app/Models/Projeto.php
+++ app/Models/Projeto.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,9 +12,16 @@ class Projeto extends Model
 
     protected $fillable = ['titulo', 'descricao', 'owner_id'];
 
+    protected $with = ['tarefas'];
+
 
     public function tarefas()
     {
         return $this->hasMany(Tarefa::class, 'projeto_id');
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
     }
 }
